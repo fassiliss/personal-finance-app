5a51271e6c898f6556d51af77572a33ccde0ef91
fassiliss <fassiliss@gmail.com>
2025-12-13 23:55:48 -0600

    Add header and footer to profile page

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,13 @@
 import AppProviders from "@/components/AppProviders";
+import MainHeader from "@/components/MainHeader";
+import MainFooter from "@/components/Footer";
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
-    return <AppProviders>{children}</AppProviders>;
+    return (
+        <AppProviders>
+            <MainHeader />
+            {children}
+            <MainFooter />
+        </AppProviders>
+    );
 }
